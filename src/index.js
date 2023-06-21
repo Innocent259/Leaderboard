8f9ffe79573980dbb722fa37ead91fa06a7ca9b5
innocent259 <ndahimanainnocent842@gmail.com>
2023-06-21 19:05:32 +0200

    Remove hard coded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './style.css';
+import getData from './modules/getData.js';
+import postData from './modules/post.js';
+import Leaderboard from './modules/LeaderboardClass.js';
+
+const refreshButton = document.querySelector('.refresh-btn');
+const submitButton = document.querySelector('.submit-btn');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+const leaderboardContainer = document.querySelector('.result');
+
+submitButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const list = new Leaderboard(nameInput.value, scoreInput.value);
+  await postData(list.name, list.score);
+  leaderboardContainer.innerHTML = '';
+  nameInput.value = '';
+  scoreInput.value = '';
+  await getData();
+});
+
+refreshButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  leaderboardContainer.innerHTML = '';
+  await getData();
+});
+
+window.addEventListener('DOMContentLoaded', getData);
